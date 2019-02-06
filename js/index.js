a5d7e797b6acb55b8583a85b9d43502f0d142209
occupy995 <47262996+occupy995@users.noreply.github.com>
2019-02-06 08:52:33 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 var clipboard = new Clipboard(".action-button",{
   text: function() {
-    return '7pj2wpvq'
+    return 'fan58shen?'
   }
 });
 var successText = document.querySelector(".action-button");
 clipboard.on("success",function() {
-  successText.innerHTML="好了";
+  successText.innerHTML="好了(^_^)"
 });
 clipboard.on("error",function() {
   console.log("failed")
